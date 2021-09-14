358c98933c48acbbba6f579f2d1a9f93b15ce1e1
Andrew <fung.andrewy@gmail.com>
2021-09-14 11:06:58 -0700

    hello.c created

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*!
+********************************
+* Main
+********************************
+* The Main function is our main loop in a C
+* program. This loop will continue until either
+* we tell the program to stop or force it to stop
+* through the operating system. For now, we will 
+* do the majority of our work from here as we learn
+* C programming. Later we will talk about other
+* functions and you will write your own functions
+*/
+int main() {
+   printf("Hello and welcome to AerE 361!\n")
+   return 0;
+}
